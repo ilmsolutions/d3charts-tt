44e40eb930d284056b1d394b3003d4d1bcfb6c19
ilmsolutions <40346358+ilmsolutions@users.noreply.github.com>
2024-10-10 10:38:02 -0500

    use join for adding d3 elements

--- src/internals/defs.ts
+++ src/internals/defs.ts
@@ -28,7 +28,9 @@ export const defs = (d3) => {
              .attr('patternUnits', 'userSpaceOnUse')
              .attr('width', 4).attr('height', 4)
              .attr('patternTransform', 'rotate(-45 2 2)')
-             .append('path').attr('d', 'M -1,2 l 6, 0')
+             .selectAll('path')
+             .data([0])
+             .join('path').attr('d', 'M -1,2 l 6, 0')
              .attr('stroke-width', 0.5);
          }
      }
